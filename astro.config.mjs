7fea57d3fbb3eaec856200a6d0cefdd601fe0375
Jose Mendez <josemmendezt@gmail.com>
2024-05-08 21:18:00 -0600

    change contact to use resend and adjust some text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
 import sitemap from '@astrojs/sitemap';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ripae.ca/',
   integrations: [tailwind(), compress(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
