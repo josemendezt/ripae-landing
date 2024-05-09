5b7a3c7f163318ba88a012c7a71ce4effab2acf1
Jose Mendez <josemmendezt@gmail.com>
2024-05-08 21:59:42 -0600

    test vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
-//  import node from "@astrojs/node";
-//  import vercel from "@astrojs/vercel/serverless";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ripae.ca/',
   integrations: [tailwind(), compress(), sitemap()],
   output: "hybrid",
-   //adapter: [vercel(), node()]
+  adapter: vercel()
 });
