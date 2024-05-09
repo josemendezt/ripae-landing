cfb38047d98a2cf63a023049d9a434af93ccfa8e
Jose Mendez <josemmendezt@gmail.com>
2024-05-08 21:51:13 -0600

    undo logic for message

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import sitemap from '@astrojs/sitemap';
+//  import node from "@astrojs/node";
+//  import vercel from "@astrojs/vercel/serverless";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ripae.ca/',
   integrations: [tailwind(), compress(), sitemap()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+   //adapter: [vercel(), node()]
 });
